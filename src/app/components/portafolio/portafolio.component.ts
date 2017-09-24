2b81bbc77e03d40a198821b87583ab50e6615474
Jorge <jorgeluiscrespo@gmail.com>
2017-09-24 08:34:37 -0300

    agregamos productos al portafolios

--- src/app/components/portafolio/portafolio.component.ts
+++ src/app/components/portafolio/portafolio.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import  { ProductosService }from "../../services/productos.service";
 @Component({
   selector: 'app-portafolio',
   templateUrl: './portafolio.component.html'
 })
 export class PortafolioComponent  {
 
-  constructor() { }
+  constructor( private _ps:ProductosService ) { }
 
 
 
